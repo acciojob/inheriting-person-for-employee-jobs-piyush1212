6baaf48205110e5ed7fa4760d94e6acd4f47deb8
Piyush Kumar Yadav <73697893+piyush1212@users.noreply.github.com>
2024-04-13 12:53:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ Employee.prototype.constructor = Employee;
 Employee.prototype.jobGreet = function() {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
+
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
